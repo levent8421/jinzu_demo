1e0bd289a0f5393596d0d841f62b2d98e11539e6
levent8421 <levent8421@outlook.com>
2022-10-30 22:20:20 +0800

    Fix image carousel

--- src/com/ImageCarousel.jsx
+++ src/com/ImageCarousel.jsx
@@ -2,17 +2,19 @@ import React, {Component} from 'react';
 import './ImageCarousel.less';
 import Image1 from '../img/home/area-1.png';
 import Image2 from '../img/home/area-2.png';
+import Image3 from '../img/home/area-3.png';
 import Icon1 from '../img/home/area-icon-1.png';
 import Icon2 from '../img/home/area-icon-2.png';
+import Icon3 from '../img/home/area-icon-3.png';
 
 const classList = ['item item-left', 'item item-right', 'item item-center'];
 
 class ImageCarousel extends Component {
     state = {
         items: [
-            {id: 0, img: Image1, className: 'item item-left', icon: Icon1, text: '金融领域'},
-            {id: 1, img: Image2, className: 'item item-center', icon: Icon2, text: '装备制造领域'},
-            {id: 2, img: Image1, className: 'item item-right', icon: Icon1, text: '三农领域'},
+            {id: 0, img: Image1, className: 'item item-left', icon: Icon1, text: '三农领域'},
+            {id: 1, img: Image2, className: 'item item-center', icon: Icon2, text: '金融领域'},
+            {id: 2, img: Image3, className: 'item item-right', icon: Icon3, text: '装备制造领域'},
         ]
     };
 
